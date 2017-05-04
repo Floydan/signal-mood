6da72b642ff25527a126fc47f2d9d77cabea319d
Sebastian <Sebastian@SEBASTIAN>
2017-05-04 13:52:24 +0200

    tabeller

---
+++ Signal-Mood/Models/Statistic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Signal_Mood.Models
+{
+    public class Statistic
+    {
+        private IEnumerable<MoodEvent> _ratings;
+        public Statistic(IEnumerable<MoodEvent> ratings)
+        {
+            _ratings = ratings;
+        }
+
+        public int DayStats(DayOfWeek weekday)
+        {
+            var rateCount = _ratings.Count(rating => rating.TimeOfRating.DayOfWeek == weekday);
+            return rateCount;
+        }
+    }
+}
