83c16429d27a9b53ff75282cadb72e7b9ebb0d41
Sebastian <Sebastian@SEBASTIAN>
2017-05-04 11:08:52 +0200

    första moodstate

---
+++ Signal-Mood/Models/MoodEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public enum MoodState
+{
+    Happy,
+    Content,
+    Dissatisfied,
+    PissedOff
+}
+namespace Signal_Mood.Models
+{
+    public class MoodEvent
+    {
+        public MoodState Rating { get; set; }
+        public DateTime TimeOfRating { get; set; }
+    }
+}
