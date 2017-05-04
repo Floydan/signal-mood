e36ebc60d99f8e1b7e820c8132b6afc28f6819e8
Joakim Höglund <joakim.hoglund@starrepublic.com>
2017-05-04 12:03:10 +0200

    updated api controller with dbcontext

--- Signal-Mood/Controllers/MoodApiController.cs
+++ Signal-Mood/Controllers/MoodApiController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Routing;
+using Signal_Mood.Data;
 using Signal_Mood.Models;
 
 namespace Signal_Mood.Controllers
@@ -12,18 +13,20 @@ namespace Signal_Mood.Controllers
 	[RoutePrefix("api/mood")]
 	public class MoodApiController : ApiController
 	{
+		private SignalMoodContext MoodContext => new SignalMoodContext();
+
 		[Route, HttpPost]
 		public bool SaveMood(MoodState mood)
 		{
-			//new MoodEvent {Rating = mood, TimeOfRating = DateTime.UtcNow};
-			return false;
+			MoodContext.MoodEvents.Add(new MoodEvent {Rating = mood, TimeOfRating = DateTime.UtcNow});
+			return MoodContext.SaveChanges() > 0;
 		}
 
 		[Route, HttpPost]
 		public IEnumerable<MoodEvent> GetEvents(DateTime fromDate, DateTime? toDate)
 		{
 			if(toDate == null) toDate = DateTime.UtcNow;
-			return Enumerable.Empty<MoodEvent>();
+			return MoodContext.MoodEvents.Where(w => w.TimeOfRating >= fromDate && w.TimeOfRating <= toDate.Value);
 		} 
 	}
 }
