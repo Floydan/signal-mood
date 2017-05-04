1a8cf5883ea00267982cebad15c6f528e05f42db
Sebastian <Sebastian@SEBASTIAN>
2017-05-04 15:42:48 +0200

    .

--- Signal-Mood/Models/Statistic.cs
+++ Signal-Mood/Models/Statistic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Razor;
 
 namespace Signal_Mood.Models
 {
@@ -16,6 +17,11 @@ namespace Signal_Mood.Models
         public int DayStats(DayOfWeek weekday)
         {
             var rateCount = _ratings.Count(rating => rating.TimeOfRating.DayOfWeek == weekday);
+            return rateCount;
+        }
+        public int DayStats(DayOfWeek weekday, MoodState state)
+        {
+            var rateCount = _ratings.Count(rating => rating.TimeOfRating.DayOfWeek == weekday && rating.Rating == state);
             return rateCount;
         }
     }
