a47c69141b3c10eb484d43524abef12e0e4b1db5
klara.nieriker <klara.nieriker@starrepublic.com>
2017-05-05 07:20:40 +0200

    commit

--- Signal-Mood/frontend/src/index.js
+++ Signal-Mood/frontend/src/index.js
@@ -3,24 +3,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import Smileys from './components/Smileys'
 import './stylesheets/main.scss'
-import { mood } from './store/reducers'
-
-const state = null;
-
-const action = {
-    type: C.SAVE_MOOD,
-    payload: {
-        "mood": 0
-    }
-}
-
-const nextState = mood(state, action)
-
-console.log(`
-    initial state: ${state}
-    action: ${JSON.stringify(action)}
-    new state: ${JSON.stringify(nextState)}
-`)
 
 render(
 	<Smileys/>,
